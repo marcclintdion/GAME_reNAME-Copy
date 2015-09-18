e27b1b080c79cba76d41412945692f63a0d9fbc6
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-17 23:36:42 -0700

    “init”

---
+++ settings.cpp
@@ -0,0 +1,19 @@
+var         marble_LIGHT_POSITION_01[]    =  {80.7258, 97.3944, 31, 1};
+
+var         marble_scaleLights        =  2.41;
+
+var         numberOfFloorTile_2m_Instances_BATCH_B        =  4;
+var         viewHeight        =  918;
+var         marble_LIGHT_POSITION_01[]    =  {80.7258, 97.3944, 31, 1};
+
+var         ambient_hardShinyBumpPlastic      =  1.354;
+
+var         scaleTurnTable         =  1;
+var         knight_POSITION[]       =  {0, -1.8, 0};
+
+var           eyeposition[]      =  {0, 0, 0, 0.0};
+var           lookAt[]        =  {0, 0, -11.85, 0.0};
+var           rotateModelWithMiddleMouse[]       =  {0, 0};
+
+var           eyeposition_SHADOW[]       =  {-2.65002, -0.38, -4.97009, 1.0};
+var         boxCount         =  1;
